7ae9a8022b318cf04e2c92200490a258012e5bbc
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-10-10 19:52:55 -0400

    added init strength routes

--- controllers/api/strengthRoutes.js
+++ controllers/api/strengthRoutes.js
@@ -1,5 +1,38 @@
 const router = require('express').Router();
+const { Strength } = require('../../models');
+const withAuth = require('../../utils/auth');
 
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newStrength = await Strength.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newStrength);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+});
 
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const strengthData = await Strength.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!strengthData) {
+        res.status(404).json({ message: 'No strength found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(strengthData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
 
 module.exports = router;
