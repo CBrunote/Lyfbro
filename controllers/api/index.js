8e4b11af4e9245e88bbb876b539d4d13d56908b7
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-10-10 19:33:45 -0400

    added route framework

---
+++ controllers/api/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const feelingRoutes = require('./feelingRoutes');
+const mindRoutes = require('./mindRoutes');
+const strengthRoutes = require('./strengthRoutes');
+const cardioRoutes = require('./cardioRoutes');
+
+router.use();
+router.use();
+router.use();
+router.use();
+router.use();
+
+
+module.exports = router;
