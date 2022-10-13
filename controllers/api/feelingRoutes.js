99a280d96afa671cb561009c06cbb5a6611c4be3
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-10-12 20:35:56 -0400

    removed put route from feeling

--- controllers/api/feelingRoutes.js
+++ controllers/api/feelingRoutes.js
@@ -24,21 +24,4 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
-  try {
-    const feelingData = await Feeling.update(req.body, {
-      where: {
-        id: req.params.id
-      }
-    });
-    if (!feelingData[0]) {
-      res.status(404).json({ message: 'No Feeling Data found with this id!'});
-      return;
-    }
-    res.status(200).json(feelingData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 module.exports = router;
